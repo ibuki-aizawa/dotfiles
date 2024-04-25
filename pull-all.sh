1c4f676ce10dd0a60a41f05c957b80fc29d8abfb
ibuki aizawa <ibuki.aizawa@sota.co.jp>
2024-04-26 04:55:36 +0900

    update

--- pull-all.sh
+++ pull-all.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-dir_path="./*"
+dir_path="./*/"
 
-# $dir_pathに合致するgitリポジトリのディレクトリをforループで処理
 for dir in $dir_path;
+
 do
   (
-    # 処理対象のgitリポジトリを表示
     echo $dir
-    # gitリポジトリのトップディレクトリへ移動
-    cd $dir
-    # git pullコマンドでリモートリポジトリの変更を取得
-    git pull
+    cd $dir && git pull
+	echo
   )
 done
 
