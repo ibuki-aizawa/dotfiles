14ef5fce12091935d5821092919fa0344a51f6d4
ibuki aizawa <ibuki.aizawa@sota.co.jp>
2025-07-12 17:30:44 +0900

    update

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,5 +1,6 @@
 
-(prefer-coding-system 'utf-8)		; utf-8
+; utf-8
+(prefer-coding-system 'utf-8)
 
 ;(setq inhibit-startup-screen t)
 ;; ウェルカムページを無効化 
@@ -10,16 +11,19 @@
 ;; 行番号を相対表示
 (setq display-line-numbers-type 'relative)
 
-;; alt space でマークセット
-(global-unset-key (kbd "M-SPC"))
-(global-set-key (kbd "M-SPC") #'set-mark-command) 
-(global-set-key (kbd "S-SPC") #'set-mark-command) 
+;;(global-unset-key (kbd "M-SPC"))
+;;(global-set-key (kbd "M-SPC") 'mark-sexp)
+
+;;(global-unset-key (kbd "C-M-SPC"))
+;;(global-set-key (kbd "C-M-SPC") 'mark-sexp)
 
 ;; C-hでバックスペース
-(global-set-key "\C-h" 'delete-backward-char)
+;(global-set-key "\C-h" 'delete-backward-char)
 
 ;; ヘルプ
-(global-set-key "\C-c\C-h" 'help-command)
+;(global-set-key "\C-c\C-h" 'help-command)
+
+;(global-set-key "\C-0" 'beginning-of-line)
 
 ;; テーマ
 (load-theme 'leuven-dark t)
