8179999c41a0c8e861ed6aa1994c3bd5dac154ed
ibuki aizawa <ibuki.aizawa@sota.co.jp>
2025-07-12 13:53:22 +0900

    update

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -1,15 +1,19 @@
 
-;; utf-8
-(prefer-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)		; utf-8
 
-;; ウェルカムページを無効化 
 ;(setq inhibit-startup-screen t)
+;; ウェルカムページを無効化 
 
 ;; 行番号を表示
 (global-display-line-numbers-mode t)
 
 ;; 行番号を相対表示
 (setq display-line-numbers-type 'relative)
+
+;; alt space でマークセット
+(global-unset-key (kbd "M-SPC"))
+(global-set-key (kbd "M-SPC") #'set-mark-command) 
+(global-set-key (kbd "S-SPC") #'set-mark-command) 
 
 ;; C-hでバックスペース
 (global-set-key "\C-h" 'delete-backward-char)
