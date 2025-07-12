29a5c819e595a2c826fd604ef2a319b64c0e67fd
ibuki aizawa <ibuki.aizawa@sota.co.jp>
2025-07-12 19:28:15 +0900

    update

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -11,19 +11,8 @@
 ;; 行番号を相対表示
 (setq display-line-numbers-type 'relative)
 
-;;(global-unset-key (kbd "M-SPC"))
-;;(global-set-key (kbd "M-SPC") 'mark-sexp)
-
-;;(global-unset-key (kbd "C-M-SPC"))
-;;(global-set-key (kbd "C-M-SPC") 'mark-sexp)
-
 ;; C-hでバックスペース
-;(global-set-key "\C-h" 'delete-backward-char)
-
-;; ヘルプ
-;(global-set-key "\C-c\C-h" 'help-command)
-
-;(global-set-key "\C-0" 'beginning-of-line)
+(global-set-key "\C-h" 'delete-backward-char)
 
 ;; テーマ
 (load-theme 'leuven-dark t)
@@ -33,3 +22,6 @@
 
 ;; 現在行を強調
 (global-hl-line-mode)
+
+;; ファイルの変更時 dired を自動更新
+(add-hook 'dired-mode-hook 'auto-revert-mode)
