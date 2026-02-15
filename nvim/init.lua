7c81ed600c9e0868f45e9072d70890f477e14e6a
Ibuki Aizawa <ibuki.aizawa@sota.co.jp>
2026-02-15 21:09:20 +0900

    feat: GitLog コマンド追加

--- nvim/init.lua
+++ nvim/init.lua
@@ -326,6 +326,82 @@ local function show_git_diff(scope)
   vim.cmd('normal! gg')
 end
 
+-- Git Log: 現在のウィンドウで表示
+local function git_log_current_file()
+    local file_path = vim.fn.expand('%:p')
+    if file_path == "" then return end
+
+    -- 分割せずに今のウィンドウで新しいバッファを開く
+    vim.cmd('enew')
+    local log_buf = vim.api.nvim_get_current_buf()
+    vim.cmd('setlocal buftype=nofile bufhidden=hide noswapfile filetype=git')
+
+    local log_cmd = "git log --pretty=format:'%h %ad %s' --date=short --follow -- " .. vim.fn.shellescape(file_path)
+    local output = vim.fn.systemlist(log_cmd)
+    vim.api.nvim_buf_set_lines(log_buf, 0, -1, false, output)
+
+    -- Enterで詳細表示（これも分割しない）
+    vim.keymap.set('n', '<CR>', function()
+        local hash = vim.fn.expand('<cword>')
+        if hash ~= "" then
+            vim.cmd('GitShow ' .. hash)
+        end
+    end, { buffer = log_buf, silent = true })
+
+    vim.keymap.set('n', 'q', ':bd<CR>', { buffer = log_buf, silent = true })
+    vim.cmd('normal! gg')
+end
+
+-- Git Show: 指定したハッシュの詳細を今のウィンドウで表示
+local function git_show_commit(opts)
+    local hash = opts.args
+    if hash == "" then
+        hash = vim.fn.expand('<cword>')
+    end
+
+    if hash == "" then
+        print("Commit ID is required")
+        return
+    end
+
+    -- 今のウィンドウで切り替え
+    vim.cmd('enew')
+    local show_buf = vim.api.nvim_get_current_buf()
+    vim.cmd('setlocal buftype=nofile bufhidden=wipe noswapfile filetype=diff')
+
+    local show_output = vim.fn.systemlist("git show " .. hash)
+    if #show_output == 0 then
+        print("Invalid Commit ID: " .. hash)
+        vim.cmd('bd')
+        return
+    end
+
+    vim.api.nvim_buf_set_lines(show_buf, 0, -1, false, show_output)
+
+    -- 詳細画面から q で戻ると Log 画面（一つ前のバッファ）に戻る
+    vim.keymap.set('n', 'q', ':bd<CR>', { buffer = show_buf, silent = true })
+    vim.cmd('normal! gg')
+end
+
+vim.api.nvim_create_user_command('GitLog', git_log_current_file, {})
+vim.api.nvim_create_user_command('GitShow', git_show_commit, { nargs = '?' })
+
+-- コマンド登録
+vim.api.nvim_create_user_command('GitLog', git_log_current_file, {})
+
+
+-- エイリアス
+vim.cmd([[ cabbrev gl GitLog ]])
+
+-- ノーマルモードで gl を叩くと GitLog を実行
+vim.keymap.set('n', 'gl', ':GitLog<CR>', { silent = true })
+
+-- コマンド登録 (引数ありを許容)
+vim.api.nvim_create_user_command('GitShow', git_show_commit, { nargs = '?' })
+
+-- エイリアス
+vim.cmd([[ cabbrev gsh GitShow ]])
+
 -- カレントバッファ版
 vim.api.nvim_create_user_command('GitDiff', function() show_git_diff('current') end, {})
 -- 全体版
