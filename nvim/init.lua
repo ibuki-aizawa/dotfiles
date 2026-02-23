ada674c2dea30901ef6f9f52b5929699814c4a72
ibuki aizawa <ibuki.aizawa@sota.co.jp>
2026-02-23 20:00:28 +0900

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,4 @@
 local vim = vim;
-local global = vim.g
 
 -- Neovim configuration file
 vim.cmd('source ~/.vimrc')
@@ -10,20 +9,3 @@ vim.cmd('set undodir=~/.nvim/undo')
 -- 読み込み
 require('core')
 require('plugins')
-
--- colorscheme
-
--- require('onedark').setup {
--- 	style = 'darker',
--- }
--- require('onedark').load()
-vim.cmd.colorscheme "catppuccin"
--- vim.cmd.colorscheme "habamax"
--- vim.cmd.colorscheme "xamabah"
-
--- vim.cmd("autocmd CursorHold *.{ts,c,cpp,hpp,hs,py} if (coc#rpc#ready() && CocHasProvider('hover') && !coc#float#has_float()) | silent call CocActionAsync('doHover') | endif")
--- vim.cmd("autocmd CursorHold * silent call CocActionAsync('highlight')")
-
--- コパイロット
-global.copilot_no_tab_map = true
-
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,4 +1,5 @@
 local vim = vim;
+local global = vim.g
 
 -- use rg for external-grep
 vim.opt.grepprg = table.concat({
@@ -14,3 +15,20 @@ vim.opt.grepprg = table.concat({
   -- [[--glob='!**.spec.ts']],
 }, ' ')
 vim.opt.grepformat = '%f:%l:%c:%m'
+
+-- colorscheme
+
+-- require('onedark').setup {
+-- 	style = 'darker',
+-- }
+-- require('onedark').load()
+vim.cmd.colorscheme "catppuccin"
+-- vim.cmd.colorscheme "habamax"
+-- vim.cmd.colorscheme "xamabah"
+
+-- vim.cmd("autocmd CursorHold *.{ts,c,cpp,hpp,hs,py} if (coc#rpc#ready() && CocHasProvider('hover') && !coc#float#has_float()) | silent call CocActionAsync('doHover') | endif")
+-- vim.cmd("autocmd CursorHold * silent call CocActionAsync('highlight')")
+
+-- コパイロット
+global.copilot_no_tab_map = true
+
