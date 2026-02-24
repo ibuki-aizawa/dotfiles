63eec4fdff9c959cb3a1708d7a218cfd8cb278e1
Ibuki Aizawa <ibuki.aizawa@sota.co.jp>
2026-02-24 19:27:42 +0900

    feat: SwitchTestFile

--- nvim/ftplugin/typescript.lua
+++ nvim/ftplugin/typescript.lua
@@ -11,3 +11,19 @@ vim.api.nvim_create_user_command('Jest', ft.run_jest_realtime, {})
 -- エイリアス（小文字でサクッと）
 -- vim.cmd([[ cabbrev jt Jest ]])
 vim.cmd([[ cabbrev jest Jest ]])
+vim.cmd([[ cabbrev test Jest ]])
+
+-- テストファイルに切り替える
+vim.api.nvim_create_user_command(
+  'SwitchTestFile',
+  function ()
+    local file = vim.fn.expand('%:p')
+    if file:match('%.spec%.ts$') then
+      file = vim.fn.expand('%:p:r:r') .. ".ts"
+    else
+      file = vim.fn.expand('%:p:r') .. ".spec.ts"
+    end
+    vim.cmd('e' .. file)
+  end,
+  {}
+);
