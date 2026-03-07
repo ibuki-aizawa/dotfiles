5ad5e43edd6ccdc9fb9e38814e62279da14c4318
Ibuki Aizawa <ibuki.aizawa@sota.co.jp>
2026-03-07 11:46:54 +0900

    fix: SelectFunctionObject

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -47,42 +47,46 @@ vim.cmd([[
 
 -- 次の数字を検索してジャンプ
 vim.api.nvim_create_user_command('SearchNumber', function()
-  -- / は検索履歴に残るため、あえて履歴を汚さない search() 関数を使う手もありますが、
-  -- 'n' キーで次々飛びたいなら、検索レジスタ (@/) を書き換えるのが正解です。
+  -- / は検索履歴に残るため、あえて履歴を汚さない search() 関数を使う手もありますが
+  -- 'n' キーで次々飛びたいので、検索レジスタ (@/) を書き換える
   vim.fn.setreg('/', [[\d\+]])
-  -- 検索を実行（n キーと同じ挙動）
   local status = pcall(vim.cmd, 'normal! n')
   if not status then
     print("数字は見つかりませんでした")
   end
 end, { desc = "Search and jump to the next number" })
 
--- 関数（カッコの塊）を行単位で検索して選択状態にする
-vim.api.nvim_create_user_command('SelectNextFunctionLines', function()
-    -- 1. ヴィジュアルモードを強制終了してノーマルモードに戻る
-    -- feedkeys を使うか、単純に一度 esc を送るイメージです
+-- 関数（波カッコの塊）を行単位で検索して選択状態にする
+vim.api.nvim_create_user_command('SelectFunctionObject', function(opts)
+    -- ヴィジュアルモードから使用した場合を考慮し、強制終了してノーマルモードに戻る
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), 'n', true)
 
     -- 少し待機（モード切り替えを確実にするため）してから実行
     vim.schedule(function()
-      -- 現在の設定を保存しておく
-      local save_wrapscan = vim.opt.wrapscan:get()
+      local is_prev = (opts or {}).args == "prev"
+      local search_op = is_prev and "?" or "/"
+      local search_pattern = [[\v\{]]
 
-      -- この関数内だけラップスキャンをオフに
+      -- 一時的にラップスキャンをオフに
+      local save_wrapscan = vim.opt.wrapscan:get()
       vim.opt.wrapscan = false
-      vim.fn.setreg('/', [[\v\{]])
 
       -- 検索実行
-      local status, err = pcall(vim.cmd, 'normal! n')
+      vim.fn.setreg('/', search_pattern)
+      local status = pcall(vim.cmd, 'normal! ' .. search_op .. search_pattern .. "\13")
 
-      -- 設定を元に戻す
       vim.opt.wrapscan = save_wrapscan
 
       if status then
-          vim.cmd('normal! va{Vo')
+        vim.cmd('normal! va{Vo')
       else
-          -- 検索失敗時のメッセージ（"search hit BOTTOM" を防ぐ）
-          print("これより先にブロックはありません")
+        print("これより先にブロックはありません")
       end
     end)
-end, { desc = "Reset selection and jump to next block" })
+end, {
+  nargs = "?",
+  desc = "Reset selection and jump to next block"
+})
+
+-- previous 版は、ヴィジュアルモード時の挙動が微妙
+vim.api.nvim_create_user_command('SelectFunctionObjectPrevious', 'SelectFunctionObject prev', {})
