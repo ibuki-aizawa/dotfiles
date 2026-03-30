e8a0e1d721707eaf76d6867969f0c3ee33d74707
Ibuki Aizawa <ibuki.aizawa@sota.co.jp>
2026-03-30 11:29:55 +0900

    feat: ft/tsreact

---
+++ nvim/ftplugin/typescriptreact.lua
@@ -0,0 +1,26 @@
+local vim = vim;
+
+local ft = require('ft.typescript')
+
+-- コマンド登録
+vim.api.nvim_create_user_command('Jest', ft.run_jest_realtime, {})
+
+-- エイリアス（小文字でサクッと）
+-- vim.cmd([[ cabbrev jt Jest ]])
+vim.cmd([[ cabbrev jest Jest ]])
+vim.cmd([[ cabbrev test Jest ]])
+
+-- テストファイルに切り替える
+vim.api.nvim_create_user_command(
+  'SwitchTestFile',
+  function ()
+    local file = vim.fn.expand('%:p')
+    if file:match('%.spec%.ts$') then
+      file = vim.fn.expand('%:p:r:r') .. ".tsx"
+    else
+      file = vim.fn.expand('%:p:r') .. ".spec.tsx"
+    end
+    vim.cmd('e' .. file)
+  end,
+  {}
+);
